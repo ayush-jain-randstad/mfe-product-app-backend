a513ac58105c62c653591e2a883288c72c85c032
manasa-randstad <manasa.j@randstaddigital.com>
2024-11-28 10:31:38 +0530

    Register user API

--- controllers/authController.js
+++ controllers/authController.js
@@ -15,7 +15,7 @@ const authController = {
             if(user != null){
                 const isPasswordValid = await bcrypt.compare(password, user.password);
                 if((email === user.email) && isPasswordValid) {
-                    const token = jwt.sign({ userId: user.id} , process.env.JWT_SECRET_KEY , { expiresIn: '5d' });
+                    const token = jwt.sign({ userId: user.id} , "process.env.JWT_SECRET_KEY" , { expiresIn: '5d' });
                     res.send({'status':200, 'message':'Login successfully','token':token, userDetails:user})
                 } else {
                     res.send({'status':201, 'message':'Email or password does not match'})
@@ -30,11 +30,30 @@ const authController = {
         
     },
 
-    register: async() => {
-        return 'register fn'
+    register: async(req, res) => {
+        const {email, password, name, confirmPassword} = req.body
+        console.log("req body", email, password, name, confirmPassword);
+
+        if (password !== confirmPassword) {
+            res.send({'status':201, 'message':'Password does not match'})
+        } else {
+            if(email && password && name && confirmPassword) {
+                const salt = await bcrypt.genSalt(10);
+                const hashedPassword = await bcrypt.hash(password, salt);
+                console.log(hashedPassword);
+                const user = await User.create({email:email, password:hashedPassword, name:name})
+                if(user) {
+                    const token = jwt.sign({ userId: user.id} , 'process.env.JWT_SECRET_KEY' , { expiresIn: '5d' });
+                    res.send({'status':200, 'message':'Register successfully','token':token, userDetails:user})
+                }
+            }else {
+                    res.send({'status':400, 'message':'Email already exist'})
+                }
+        }
+
+
+        }
     }
 
-
-}
 
 export default authController
