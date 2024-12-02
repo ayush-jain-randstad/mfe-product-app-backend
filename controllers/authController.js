631e341a940803456614fbf6ae30a89951b506a3
Jain <Ayush.Jain@randstaddigital.com>
2024-12-02 16:13:32 +0530

    get access token from refresh token task is done

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,7 +3,8 @@ import bcrypt from 'bcrypt'
 import User from "../models/User.js";
 import jwt from 'jsonwebtoken'
 import response from "../helpers/response.js";
-
+import { generateAccessToken, generateRefreshToken } from "../helpers/generateTokens.js";
+import RefreshToken from "../models/RefreshToken.js";
 const authController = {
 
     login: async (req, res) => {
@@ -15,8 +16,14 @@ const authController = {
             if(user != null){
                 const isPasswordValid = await bcrypt.compare(password, user.password);
                 if((email === user.email) && isPasswordValid) {
-                    const token = jwt.sign({ userId: user.id} , process.env.JWT_SECRET_KEY , { expiresIn: '5d' });
-                    res.status(200).send({'status':200, 'message':'Login successfully','token':token, userDetails:user})
+                    // const token = jwt.sign({ userId: user.id} , process.env.JWT_SECRET_KEY , { expiresIn: '5d' });
+                    const accessToken = await generateAccessToken(user.id)
+                    const refreshToken = await generateRefreshToken(user.id)
+                    await RefreshToken.create({
+                        user_id: user.id,
+                        refresh_token: refreshToken
+                    })
+                    res.status(200).send({'status':200, 'message':'Login successfully','accessToken':accessToken,'refreshToken': refreshToken, userDetails:user})
                 } else {
                     return response.sendUnauthorized(res,'Email or password does not match')
                 }
@@ -29,6 +36,31 @@ const authController = {
         
     },
 
+    refreshToken:async(req, res) => {
+        const {refreshToken} = req.body;
+        if (!refreshToken) return response.sendUnauthorized(res,'Refresh Token not available');
+        try {
+            const checkRefreshToken = await RefreshToken.findOne({
+                where: { refresh_token:refreshToken },
+                include: User
+            })
+            console.log('checkRefreshToken',checkRefreshToken);
+            if(!checkRefreshToken) return response.sendUnauthorized(res,'Refresh Token not found in Database');
+            jwt.verify(refreshToken, process.env.JWT_REFRESH_KEY, async (err, user) => {                    
+                if (err) return response.sendError(res, 'Can not verify refresh token')
+          
+                const newAccessToken = await generateAccessToken(user.userId); 
+                
+                return res.status(200).send({'status':200,'accessToken':newAccessToken,})
+              });
+        
+        } catch (error) {
+            console.error('Error refreshing access token:', error);
+            return response.sendError(res, 'Error refreshing access token')
+        }
+        
+        
+    }
 
 }
 
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,5 +4,5 @@ import authController from "../controllers/authController.js";
 const router = express.Router()
 
 router.post('/login', authController.login)
-
+router.post('/refresh', authController.refreshToken)
 export default router
